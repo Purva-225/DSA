9d796aed747131a76cefdd854c55aef69fd020b0
Purva nigade <purvanigade6@gmail.com>
2026-02-12 17:50:52 +0530

    longestBalanced function to find max length

---
+++ 3713.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+     bool checkbalanced(vector<int> freq){
+        int common = 0; 
+        for(int i =0; i<26; i++){
+            if(freq[i] == 0) continue;
+            if(common == 0)
+            common = freq[i];
+            else if(freq[i] != common){
+                return false;
+            }
+        }
+        return true;
+     }
+    int longestBalanced(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        for(int i =0; i<n; i++){
+          vector<int> freq(26,0);
+
+          for(int j = i; j<n; j++){
+             freq[s[j] -'a']++;
+
+             if (checkbalanced(freq)){
+                maxLen = max(maxLen, j-i+1);
+             }
+          }
+        }
+        return maxLen;
+    }
+};
