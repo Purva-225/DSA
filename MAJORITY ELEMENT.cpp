225ef3d7f0d9538e220b7b450fe0392580f19fa0
Purva nigade <purvanigade6@gmail.com>
2026-03-23 00:06:09 +0530

    Add hashing approach for majority element detection
    Implemented a better approach to find the majority element using hashing.

--- MAJORITY ELEMENT.cpp
+++ MAJORITY ELEMENT.cpp
@@ -17,6 +17,24 @@ public:
     }
 };
 
+// Better approach using hashing :
+
+int n = nums.size();
+
+      map<int,int> mpp;
+
+      for(int i =0; i <n; i++){
+        mpp[nums[i]]++;
+      }
+      for(auto it : mpp){
+        if(it.second > (nums.size()/2)){
+            return it.first;
+        }
+      }
+      return -1; 
+}
+};
+
 // optimal one : 
 
 class Solution {
