75e60415ae20958454e58dd5ec0558ae7d8ac2a9
Purva nigade <purvanigade6@gmail.com>
2026-03-22 22:52:12 +0530

    Implement Boyer-Moore Voting Algorithm for majority element

--- MAJORITY ELEMENT.cpp
+++ MAJORITY ELEMENT.cpp
@@ -16,3 +16,35 @@ public:
        return -1;
     }
 };
+
+// optimal one : 
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+       int n = nums.size();
+        int cnt = 0; 
+       int elem;
+
+       for(int i = 0; i<n; i++){
+          if(cnt == 0){
+            cnt = 1; 
+            elem = nums[i];
+          }else if(nums[i] == elem){
+            cnt++;
+          }else {
+            cnt--; 
+          }
+       }
+       int cnt1 = 0; 
+       for(int i = 0; i<n; i++){
+        if(nums[i] == elem){
+            cnt1++; 
+        } 
+        if(cnt1 > (n / 2)){
+            return elem;
+        }
+       }
+       return -1; 
+    }
+};
