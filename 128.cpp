47329e086a95e564524d2e6b8b90531d1b0816b2
Purva nigade <purvanigade6@gmail.com>
2026-03-31 00:07:18 +0530

    LONGEST CONSICATIVE.28.cpp

--- 128.cpp
+++ 128.cpp
@@ -1,3 +1,52 @@
+// BRUTE APPROACH : WHICH GIVES TLE. 
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = 0;
+
+        for(int i = 0; i < n; i++) {
+            int x = nums[i];
+            int cnt = 1;
+
+           
+            while(find(nums.begin(), nums.end(), x + 1) != nums.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+
+            maxi = max(maxi, cnt);
+        }
+
+        return maxi;
+    }
+};
+/* BETTER APPROACH : - 
+    class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+       if(nums.size() == 0) return 0; 
+       sort(nums.begin(), nums.end());
+       int LastSmaller = INT_MIN; 
+       int cnt = 0;
+       int longest = 1;
+       int n = nums.size(); 
+
+       for(int i =0; i<n; i++){
+          if(nums[i]-1 == LastSmaller){
+            cnt += 1;
+            LastSmaller = nums[i];
+          }
+          else if(LastSmaller != nums[i]){
+            cnt = 1; 
+            LastSmaller = nums[i];
+          }
+          longest = max(longest,cnt);
+       }
+      return longest;
+    }
+}; */ 
+/* OPTIMAL : 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
@@ -19,4 +68,4 @@ public:
         }
         return longest;
     }
-};
+}; */
