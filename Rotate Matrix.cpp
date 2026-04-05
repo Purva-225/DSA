3e75ef35bcbae5e8fd9ca386477d91c04948219b
Purva nigade <purvanigade6@gmail.com>
2026-04-05 22:50:27 +0530

    matrix rotation method
    Added an optimized approach for rotating the matrix in place.

--- Rotate Matrix.cpp
+++ Rotate Matrix.cpp
@@ -1,3 +1,20 @@
+//Brutal Approach:
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+      int n = matrix.size();
+       vector<vector<int>> ans(n, vector<int>(n));
+
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j<n; j++){
+           ans[j][n-1-i] = matrix[i][j];
+        }
+      }
+      matrix = ans;
+    }
+};
+
+// Optimized: 
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
