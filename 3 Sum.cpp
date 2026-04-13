88183084cde8f77dd0d2b3955138584d155fc288
Purva nigade <purvanigade6@gmail.com>
2026-04-13 16:55:48 +0530

    Add threeSum function with multiple implementations
    Implemented threeSum function using three different approaches: brute force, hash set, and optimal two-pointer technique.

---
+++ 3 Sum.cpp
@@ -0,0 +1,101 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+// BRUTAL APPROACH;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Better one [ Only we had applied here an formula ]
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+          
+             set<vector<int>> st;
+             for(int i = 0; i<n; i++){
+                set<int> hashset;
+                for(int j = i+1; j<n; j++){
+                    int k = -(nums[i] + nums[j]);
+                        if(hashset.find(k) != hashset.end()){
+                           vector<int> temp = {nums[i], nums[j],k};
+                            sort(temp.begin(), temp.end());
+                            st.insert(temp);
+                        }
+                         hashset.insert(nums[j]);
+                      }
+                 }
+            vector<vector<int>> ans(st.begin(), st.end());
+            return ans; 
+       }
+};         
+
+// Optimal
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+          vector<vector<int>> ans; 
+          sort(nums.begin(), nums.end()); 
+
+            for(int i = 0; i<n; i++){
+              if(i > 0 && nums[i] == nums[i -1]) continue; 
+                int j = i + 1; 
+                int k = n-1; 
+              while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0 ){
+                  k--; 
+                }
+                else{
+                vector<int> temp = {nums[i], nums[j], nums[k]};
+                ans.push_back(temp);
+                j++;
+                k--; 
+                while(j < k && nums[j] == nums[j-1]) j++; 
+                while(j < k && nums[k] == nums[k+1]) k--; 
+                }
+              }
+          } 
+          return ans; 
+       }
+};                   
+
