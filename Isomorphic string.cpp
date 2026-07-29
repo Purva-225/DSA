84419dec0fe7f3d34bca2e1b5a3145ea1baa222e
Purva nigade <purvanigade6@gmail.com>
2026-07-29 13:10:37 +0530

    Add isIsomorphic function in Isomorphic string.cpp
    Implement function to check if two strings are isomorphic.

---
+++ Isomorphic string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+       unordered_map<char,int> mp1; 
+        unordered_map<char,int> mp2; 
+     
+       for(int i = 0; i<n; i++){
+        char ch1 = s[i]; 
+        char ch2 = t[i]; 
+
+         if(mp1.find(ch1) != mp1.end() && mp1[ch1] != ch2 ||
+          mp2.find(ch2) != mp2.end() && mp2[ch2] != ch1){
+            return false; 
+           }
+            mp1[ch1] = ch2; 
+            mp2[ch2] = ch1; 
+        }
+        return true; 
+    }
+};
+
+
+ // char in s be replaced to get T
+       //SWAP EACH CHAR WITH T 
+       // IF APRAT FROM CHAR I FOUND SPEAIAL VARIABLES OR NUMBERS RETURN FALSE 
